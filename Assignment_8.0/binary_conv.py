60cdb7609a4b214a4c9bc932da25145f6d53bf4d
mbougasp-dot <mbougasp@mail.lcc.edu>
2025-12-04 01:06:35 -0500

    Again, one more step ahead

--- Assignment_8.0/binary_conv.py
+++ Assignment_8.0/binary_conv.py
@@ -1,3 +1,7 @@
 ### Before starting the first thing to do is to link all my files together by importing the different functions I created in each file.
 ### Let's import the function from my type_input file to use it here.
-from input_type import detect_number_type(num_str)
+from type_input import detect_number_type
+
+### Now let's define our function to convert the number entered by the user to binary.
+def convert_to_binary(num_str):
+    """convert either integer or hex string to binary Instruction code !!."""
--- Assignment_8.0/type_input.py
+++ Assignment_8.0/type_input.py
@@ -2,4 +2,8 @@
 ### Let's define our function first to detect the type of number entered by the user.
 def detect_number_type(num_str):
     """Return 'hex' if the number from the user input starts with 0x, otherwise return 'integer'. """
-    
+    if num_str.startswith("0x") or num_str.startswith("0X"): ### This check whether the inout is a capital or small letter. It makes the input action case sensitive.
+        return "hex"
+    else:
+        return "integer"
+    ### From this function we will be able to detect the type of the entry and use it in the main program for the convertion to binary.
