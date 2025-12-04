a3deb5261e92f3a639a62768c48c74f5672aec2d
mbougasp-dot <mbougasp@mail.lcc.edu>
2025-12-04 00:59:49 -0500

    defining the detect function and how it should work

--- Assignment_8.0/binary_conv.py
+++ Assignment_8.0/binary_conv.py
@@ -1,1 +1,3 @@
 ### Before starting the first thing to do is to link all my files together by importing the different functions I created in each file.
+### Let's import the function from my type_input file to use it here.
+from input_type import detect_number_type(num_str)
--- Assignment_8.0/type_input.py
+++ Assignment_8.0/type_input.py
@@ -1,2 +1,5 @@
 ### Before starting the first thing to do is to link all my files together by importing the different functions I created in each file.
 ### Let's define our function first to detect the type of number entered by the user.
+def detect_number_type(num_str):
+    """Return 'hex' if the number from the user input starts with 0x, otherwise return 'integer'. """
+    
