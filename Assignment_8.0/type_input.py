090206a9fd8c99a6c1a079188ee61c06f86fdeff
mbougasp-dot <mbougasp@mail.lcc.edu>
2025-12-04 00:54:07 -0500

    I state my idea clearly

--- Assignment_8.0/type_input.py
+++ Assignment_8.0/type_input.py
@@ -1,1 +1,2 @@
 ### Before starting the first thing to do is to link all my files together by importing the different functions I created in each file.
+### Let's define our function first to detect the type of number entered by the user.
